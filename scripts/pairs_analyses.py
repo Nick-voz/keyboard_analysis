047fbfdf0210474ef05cdf9b534c2ff392c2d910
«Nick-voz» <nik.vozisow@gmail.com>
2024-12-21 12:22:57 +0300

    Divided the code into functions to improve readability and maintainability

--- scripts/pairs_analyses.py
+++ scripts/pairs_analyses.py
@@ -1,4 +1,6 @@
 import sqlite3
+from datetime import date
+from datetime import datetime
 from datetime import time
 from string import ascii_letters
 from string import ascii_lowercase
@@ -6,6 +8,7 @@ from string import ascii_uppercase
 from string import punctuation
 from time import sleep
 from typing import Any
+from typing import Literal
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -13,6 +16,8 @@ import seaborn as sns
 from env import DB_PATH
 from env import MEDIA_ROOT
 from env import RUSSIAN_LETTERS
+
+NAME = None
 
 
 def load_keys_df() -> pd.DataFrame:
@@ -26,12 +31,14 @@ def load_keys_df() -> pd.DataFrame:
 
 
 def get_pyres(
-    keys_df: pd.DataFrame, allolewd_keys, case_ignore=False
+    keys_df: pd.DataFrame, allolewd_keys, case_ignore=True
 ) -> list[tuple[Any, Any]]:
     _pyres = []
     for i in range(len(keys_df) - 1):
         first_key = keys_df.iloc[i]["key"]
         second_key = keys_df.iloc[i + 1]["key"]
+        if first_key is None or second_key is None:
+            continue
 
         if first_key not in allolewd_keys:
             continue
@@ -45,41 +52,75 @@ def get_pyres(
     return _pyres
 
 
-keys_df = load_keys_df()
-pyres = get_pyres(keys_df, ascii_letters)
-# pyres = get_pyres(keys_df, RUSSIAN_LETTERS)
-# pyres = get_pyres(keys_df, RUSSIAN_LETTERS, case_ignore=True)
-# pyres = get_pyres(keys_df, punctuation)
+def get_filtered_pyres(filter: Literal["asci", "ru", "punctuation"]):
+    global NAME  # pylint: disable=global-statement
+    keys_df = load_keys_df()
+    if filter == "asci":
+        pyres = get_pyres(keys_df, ascii_letters)
+        NAME = "ascii_letters"
+    elif filter == "ru":
+        pyres = get_pyres(keys_df, RUSSIAN_LETTERS)
+        NAME = "russian"
+    else:
+        pyres = get_pyres(keys_df, punctuation)
+        NAME = "punctuation"
+    return pyres
 
 
-pyres_df = pd.DataFrame(pyres, columns=["first_key", "second_key"])
-
-frequency_table = pyres_df.pivot_table(
-    index="first_key", columns="second_key", aggfunc="size", fill_value=0
-)
-
-top_keys = frequency_table.sum(axis=1).nlargest(30).index
-
-filtered_frequency_table = frequency_table.loc[top_keys, top_keys]
-
-plt.figure(figsize=(20, 18))
-sns.set(font_scale=1)
-sns.heatmap(
-    filtered_frequency_table,
-    annot=True,
-    fmt="d",
-    cmap="YlGnBu",
-    cbar_kws={"label": "Частота"},
-)
-
-plt.title("Частота пар нажатий клавиш", fontsize=16)
-
-plt.xlabel("Следующая клавиша", fontsize=12)
-plt.ylabel("Текущая клавиша", fontsize=12)
-
-plt.yticks(rotation=0)
+def prepare_data(pyres):
+    pyres_df = pd.DataFrame(pyres, columns=["first_key", "second_key"])
+    frequency_table = pyres_df.pivot_table(
+        index="first_key", columns="second_key", aggfunc="size", fill_value=0
+    )
+    keys = frequency_table.sum(axis=1).index
+    return frequency_table.loc[keys, keys]
 
 
-plt.savefig(f"{MEDIA_ROOT}pairs.png", bbox_inches="tight")
-plt.show()
-plt.close()
+def create_visualization(filtered_frequency_table):
+    plt.figure(figsize=(20, 18))
+    sns.set_theme(font_scale=1)
+    sns.heatmap(
+        filtered_frequency_table,
+        annot=True,
+        fmt="d",
+        cmap="YlGnBu",
+        cbar_kws={"label": "Частота"},
+    )
+
+    plt.title("Частота пар нажатий клавиш", fontsize=16)
+
+    plt.xlabel("Следующая клавиша", fontsize=12)
+    plt.ylabel("Текущая клавиша", fontsize=12)
+
+    plt.yticks(rotation=0)
+
+
+def save_visalization():
+    _date = datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+    plt.savefig(f"{MEDIA_ROOT}pairs_{NAME}_{_date}.png", bbox_inches="tight")
+    plt.close()
+
+
+def ask_for_filter() -> Literal["ru", "asci", "punctuation"]:
+    options: dict[str, Literal["ru", "asci", "punctuation"]] = {
+        "0": "ru",
+        "1": "asci",
+        "2": "punctuation",
+    }
+    value = input(
+        "chose allowed keys: ru(0), ascii letters(1), punctuation(2): "
+    )
+    if value not in options:
+        return ask_for_filter()
+    return options[value]
+
+
+def main():
+    _filter = ask_for_filter()
+    pyres = get_filtered_pyres(_filter)
+    create_visualization(prepare_data(pyres))
+    save_visalization()
+
+
+if __name__ == "__main__":
+    main()
